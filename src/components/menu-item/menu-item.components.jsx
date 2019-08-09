af4e495b88dc89aadf6f04e9c898826c3ce318aa
Zans Litinskis <Zans.Litinskis@vertumotors.com>
2019-08-09 08:57:30 +0100

    split into components

---
+++ src/components/menu-item/menu-item.components.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+//CSS
+import './menu-item.styles.scss';
+
+
+
+
+export const MenuItem = ({title, imageUrl,size}) => {
+
+    return(
+        <div className={`${size ? size : ''} menu-item`} style={{
+            backgroundImage: `url(${imageUrl})`,
+        }}>
+        <div className="content">
+            <h1 className="title">
+                {title}
+            </h1>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
+    )
+
+}
---
+++ src/components/pages/homepage/homepage.components.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+//Components
+import { Directory } from '../../directory/directory.components';
+// Styles
+import './homepage.styles.scss'
+
+
+
+export const HomePage =() => {
+
+
+
+    return(
+        
+        <div className="homepage">
+            <Directory/>
+        </div>
+    ) 
+
+    
+}
