7c3cdb3b82f6d4a65ab3dc0fcaf52c3b26747e62
Zans Litinskis <Zans.Litinskis@vertumotors.com>
2019-08-09 19:47:03 +0100

    styling menu

--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -9,9 +9,10 @@ import './menu-item.styles.scss';
 export const MenuItem = ({title, imageUrl,size}) => {
 
     return(
-        <div className={`${size ? size : ''} menu-item`} style={{
+        <div className={`${size ? size : ''} menu-item`} >
+            <div className="background-image" style={{
             backgroundImage: `url(${imageUrl})`,
-        }}>
+        }}></div>
         <div className="content">
             <h1 className="title">
                 {title}
