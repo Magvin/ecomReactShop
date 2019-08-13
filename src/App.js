84ddae38f09b43d5d546366675e55d66b24fa46c
Magvin <zans.litinskis@gmail.com>
2019-08-13 08:24:43 +0100

    SignIn component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 //Components
 import { HomePage } from './components/pages/homepage/homepage.components';
 import ShopPage  from './components/pages/shop/shop.components';
+import SignInAndSignUp from './components/pages/sigin-and-signup/sigin-and-signup.component';
 
 
 
@@ -12,8 +13,9 @@ function App() {
   return (
     <div>
       <Switch>
-      <Route exact path='/' component={ HomePage }/>
+      <Route exact path='/' component={ HomePage } />
       <Route exact path='/shop' component={ ShopPage } />
+      <Route exact path='/signin' component={ SignInAndSignUp } />
       </Switch>
     </div>
   );
