3f58aeb4c1e45577ef13da90625b7fff2121db84
Magvin <zans.litinskis@gmail.com>
2019-08-28 08:07:19 +0100

    auth with email

--- src/components/pages/sigin-and-signup/sigin-and-signup.component.jsx
+++ src/components/pages/sigin-and-signup/sigin-and-signup.component.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import SignIn from '../../sign-in/sign-in.component';
 // CSS 
 import './sigin-and-signup.styles.scss';
+import SignUpComponent from '../../sign-up/sign-up.components';
 
 const SignInAndSignUpPage = () => (
     <div className="sign-in-and-sign-up">
         <SignIn/>
+        <SignUpComponent/>
     </div>
 )
 
