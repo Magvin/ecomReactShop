9d710c864eb51c77f07be02835860d3f41bf23fa
Magvin <zans.litinskis@gmail.com>
2019-09-03 08:01:01 +0100

    redirect if user is signin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch,Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 
@@ -35,6 +35,8 @@ class App extends React.Component {
       }
       setCurrentUser(userAuth)
     })
+    
+
   }
   
   componentWillUnmount() {
@@ -44,21 +46,27 @@ class App extends React.Component {
 
 
   render() {
+    
   return (
     <div>
       <Header/>
       <Switch>
       <Route exact path='/' component={ HomePage } />
       <Route exact path='/shop' component={ ShopPage } />
-      <Route exact path='/signin' component={ SignInAndSignUp } />
+     
+      <Route exact path='/signin' render={()=> this.props.currentUser ? (<Redirect to ="/"/>) : (<SignInAndSignUp/>)} /> 
       </Switch>
     </div>
   );
   }
 }
 
+const mapStateToProps =({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
