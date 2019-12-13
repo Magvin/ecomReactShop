91f71b6d0ffedf32cce8c88b244d9689928a1a51
Magvin <zans.litinskis@gmail.com>
2019-12-13 09:29:51 +0000

    Fixing selection

--- src/components/pages/collection/collection.components.jsx
+++ src/components/pages/collection/collection.components.jsx
@@ -1,33 +1,27 @@
-import React from 'react';
-import CollectionItem from '../../collection-item/collection-item.component';
-import './collection.styles.scss';
+import React from "react";
+import CollectionItem from "../../collection-item/collection-item.component";
+import "./collection.styles.scss";
 
 // Redux
-import { connect } from 'react-redux';
-import { selectCollection } from '../../../redux/shop/shop.selectors';
-
+import { connect } from "react-redux";
+import { selectCollection } from "../../../redux/shop/shop.selectors";
 
 const CollectionPage = ({collection}) => {
-   
-    const { title, items } = collection;
-    return (
-        <div className="collection-page">
-<h2 className="title">{title}</h2>
-        <div className="items">
-        {
-                items.map((item)=>(
-                    <CollectionItem key={item.id} item={item}  />
-                ))
-                }
-        </div>
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
-    )
-}
+  );
+};
 
-const mapStateProps = (state,ownProps) => ({
-    collection:selectCollection(ownProps.match.params.collectionId)(state)
-})
-
-
+const mapStateProps = (state, ownProps) => (console.log(state),{
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
 
 export default connect(mapStateProps)(CollectionPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,15 +1,17 @@
 import { createSelector } from "reselect";
 
-const shop = state => state.shop;
+const selectShop = state => state.shop;
 
-export const selectShopSections = createSelector([shop], shop => shop);
+export const selectShopSections = createSelector([selectShop], shop => shop);
 
-export const selectCollectionForPrievew = createSelector(
+export const selectCollectionsForPreview = createSelector(
   [selectShopSections],
-  collections => Object.keys(collections).map(key=> collections[key])
-)
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
 export const selectCollection = collectionUrlParam =>
-  createSelector(
-    [selectShopSections],
-    collections => collections[collectionUrlParam]
+  createSelector([selectShopSections], collections =>
+    {return collections.find(collection => {
+      return collection.routeName === collectionUrlParam;
+    })}
   );
