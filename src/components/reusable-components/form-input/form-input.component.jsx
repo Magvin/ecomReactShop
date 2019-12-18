b4a7ac826dda3991b6a4cf4f678f985415e3f89b
Magvin <zans.litinskis@gmail.com>
2019-12-18 21:09:40 +0000

    Styled Components: Rewrite Form Input css in to CSS in JS

--- src/components/reusable-components/form-input/form-input.component.jsx
+++ src/components/reusable-components/form-input/form-input.component.jsx
@@ -1,15 +1,11 @@
-import React from 'react'
-import './form-input.styles.scss'
-import { FormGroupContainer, FormInputContainer } from './form-input.styles';
+import React from "react";
+import { FormGroupContainer, FormInputContainer, FormInputLabelContainer } from "./form-input.styles";
 
-const FormInput =( {handleChange,label,...otherProps} ) => (
-    <FormGroupContainer>
-        <FormInputContainer onChange={handleChange} {...otherProps}/>
-        {
-            label ? (<label className={`${otherProps.value.length} ? 'shrink' :'' form-input-label`}>{ label }</label>) : null
-        }
-    </FormGroupContainer>
-)
-
+const FormInput = ({ handleChange, label, ...props }) => (
+  <FormGroupContainer>
+    <FormInputContainer onChange={handleChange} {...props} />
+    {label ? <FormInputLabelContainer {...props}>{label}</FormInputLabelContainer> : null}
+  </FormGroupContainer>
+);
 
 export default FormInput;
--- src/components/reusable-components/form-input/form-input.styles.jsx
+++ src/components/reusable-components/form-input/form-input.styles.jsx
@@ -1,43 +1,60 @@
 import styled, { css } from "styled-components";
 
 const localVariables = {
-subColor: '#808080',
-mainColor: 	'#000000'
+  subColor: "#808080",
+  mainColor: "#000000"
+};
 
-}
-
-
-const shinkLabel = () => `
-    top: -14px;
-    font-size: 12px;
-    color: ${localVariables.mainColor}
-
-`
-
+const shinkLabel = css`
+  top: -14px;
+  font-size: 12px;
+  color: ${localVariables.mainColor};
+`;
 
 export const FormGroupContainer = styled.div`
   position: relative;
   margin: 45px 0;
 `;
 
+const getLabelStyles = props => {
+  if (props.value.length > 0) {
+    return shinkLabel;
+  } else {
+    return;
+  }
+};
+export const FormInputLabelContainer = styled.label`
+  color: ${localVariables.subColor};
+  font-size: 16px;
+  font-weight: normal;
+  position: absolute;
+  pointer-events: none;
+  left: 5px;
+  top: 10px;
+  transition: 300ms ease all;
+  ${getLabelStyles};
+`;
 export const FormInputContainer = styled.input`
   background: none;
-    background-color: white;
-    color: ${localVariables.subColor};
-    font-size: 18px;
-    padding: 10px 10px 10px 5px;
-    display: block;
-    width: 100%;
-    border: none;
-    border-radius: 0;
-    border-bottom: 1px solid ${localVariables.mainColor};;
-    margin: 25px 0;
-
-    &:focus {
-      outline: none;
+  background-color: white;
+  color: ${localVariables.subColor};
+  font-size: 18px;
+  padding: 10px 10px 10px 5px;
+  display: block;
+  width: 100%;
+  border: none;
+  border-radius: 0;
+  border-bottom: 1px solid ${localVariables.mainColor};
+  margin: 25px 0;
+  ${props => {
+    if (props.type === "password") {
+      return `letter-spacing: 0.3em`;
     }
-
-    &:focus ~ .form-input-label {
-      ${shinkLabel}
-    }
-`
+  }}
+  &:focus {
+    outline: none;
+  }
+  &:focus ~ label {
+    ${shinkLabel}
+  }
+`;
