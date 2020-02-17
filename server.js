4fe979164543e85d2fc2c820e31f99e82645278d
zans.litinskis@gmail.com <zans.litinskis@gmail.com>
2020-02-17 20:34:52 +0000

    Stripe - Stripe backend

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require('path');
 
 if(process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+// @ts-ignore
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -19,7 +22,22 @@ if(process.env.NODE_ENV === 'production') {
     app.get('*', function(req,res){
         res.sendFile(path.join(__dirname,'client/build', 'index.html'))
     });
-}
+};
+
+app.post('/payment', (req, res) => {
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: 'usd'
+    };
+    stripe.charges.create(body, (stripeErr, stripeRes) =>{
+        if(stripeErr) {
+            res.status(500).send({error: stripeErr});
+        } else {
+            res.status(200).send({success: stripeRes});
+        } 
+    })
+})
 
 app.listen(port,error => {
     if(error) throw error;
